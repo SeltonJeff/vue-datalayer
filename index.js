74cb9632ceb8d8684e3463b220e96c86b5ba4664
Selton Jeffeson <selton.jeffeson@catskillet.com>
2021-12-16 12:26:15 -0300

    feat: ajustes index.js

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
-import Vue from 'vue'
-
-const handleSetOnDataLayer = (currentDLObject) => {
-	const requiredDLObject = currentDLObject
-	if (window.dataLayer) window.dataLayer.push(requiredDLObject)
-	else {
-		window.dataLayer = []
-		window.dataLayer.push(requiredDLObject)
-	}
-}
-
-const handleClickObserver = (el, currentDLObject) => {
-	const keys = Object.keys(currentDLObject)
-	const values = Object.values(currentDLObject)
-	const requiredDLObject = {}
-	el.onclick = () => {
-		for (const i in Object.keys(currentDLObject)) {
-			if (keys[i] !== 'click' && keys[i] !== 'hover')
-				requiredDLObject[keys[i]] = values[i]
-		}
-		requiredDLObject.event = 'click'
-		handleSetOnDataLayer(requiredDLObject)
-	}
-}
-
-const handleMouseOverObserver = (el, currentDLObject) => {
-	const keys = Object.keys(currentDLObject)
-	const values = Object.values(currentDLObject)
-	const requiredDLObject = {}
-	el.onmouseover = () => {
-		for (const i in Object.keys(currentDLObject)) {
-			if (keys[i] !== 'click' && keys[i] !== 'hover')
-				requiredDLObject[keys[i]] = values[i]
-		}
-		requiredDLObject.event = 'mouseOver'
-		handleSetOnDataLayer(requiredDLObject)
-	}
-}
-
-const handleComposeDLObject = (el, customDLObject) => {
-	const defaultDLObject = {
-		click: true,
-		hover: false,
-		action: '',
-		target: el,
-		category: el.tagName.toLowerCase(),
-		page: el.baseURI,
-	}
-	return Object.assign(defaultDLObject, customDLObject)
-}
-
-Vue.directive('DLObject', {
-	bind(el, { value }) {
-		const customDLObject = value
-		const currentDLObject = handleComposeDLObject(el, customDLObject)
-		if (currentDLObject !== null && currentDLObject.click === true)
-			handleClickObserver(el, currentDLObject)
-		if (currentDLObject !== null && currentDLObject.hover === true)
-			handleMouseOverObserver(el, currentDLObject)
-	},
-})
+"use strict";
+exports.__esModule = true;
+var vue_1 = require("vue");
+var handleSetOnDataLayer = function (currentDLObject) {
+    var requiredDLObject = currentDLObject;
+    if (window.dataLayer)
+        window.dataLayer.push(requiredDLObject);
+    else {
+        window.dataLayer = [];
+        window.dataLayer.push(requiredDLObject);
+    }
+};
+var handleClickObserver = function (el, currentDLObject) {
+    var keys = Object.keys(currentDLObject);
+    var values = Object.values(currentDLObject);
+    var requiredDLObject = {};
+    el.onclick = function () {
+        for (var i in Object.keys(currentDLObject)) {
+            if (keys[i] !== 'click' && keys[i] !== 'hover')
+                requiredDLObject[keys[i]] = values[i];
+        }
+        requiredDLObject.event = 'click';
+        handleSetOnDataLayer(requiredDLObject);
+    };
+};
+var handleMouseOverObserver = function (el, currentDLObject) {
+    var keys = Object.keys(currentDLObject);
+    var values = Object.values(currentDLObject);
+    var requiredDLObject = {};
+    el.onmouseover = function () {
+        for (var i in Object.keys(currentDLObject)) {
+            if (keys[i] !== 'click' && keys[i] !== 'hover')
+                requiredDLObject[keys[i]] = values[i];
+        }
+        requiredDLObject.event = 'mouseOver';
+        handleSetOnDataLayer(requiredDLObject);
+    };
+};
+var handleComposeDLObject = function (el, customDLObject) {
+    var defaultDLObject = {
+        click: true,
+        hover: false,
+        action: '',
+        target: el,
+        category: el.tagName.toLowerCase(),
+        page: el.baseURI
+    };
+    return Object.assign(defaultDLObject, customDLObject);
+};
+vue_1["default"].directive('DLObject', {
+    bind: function (el, _a) {
+        var value = _a.value;
+        var customDLObject = value;
+        var currentDLObject = handleComposeDLObject(el, customDLObject);
+        if (currentDLObject !== null && currentDLObject.click === true)
+            handleClickObserver(el, currentDLObject);
+        if (currentDLObject !== null && currentDLObject.hover === true)
+            handleMouseOverObserver(el, currentDLObject);
+    }
+});
