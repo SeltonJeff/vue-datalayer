ca1420b08c8130cd641fceeec204f8f68e5001a7
Selton Jeffeson <selton.jeffeson@catskillet.com>
2021-12-16 12:20:54 -0300

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# vue-datalayer
-Abstract implementation for your project.
+# Vue-Datalayer for Nuxt.js
+
+vue-datalayer is a plugin for direct use, its functionality is based on the need to implement clicks and mouseOver observers for traffic management for marketing.
+
+In nuxt
---
+++ index.ts
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+declare const window: any;
+declare const Object: any;
+
+const handleSetOnDataLayer = (currentDLObject: any) => {
+	const requiredDLObject = currentDLObject
+	if (window.dataLayer) window.dataLayer.push(requiredDLObject)
+	else {
+		window.dataLayer = []
+		window.dataLayer.push(requiredDLObject)
+	}
+}
+
+const handleClickObserver = (el: HTMLElement, currentDLObject: any) => {
+	const keys = Object.keys(currentDLObject)
+	const values = Object.values(currentDLObject)
+	const requiredDLObject: any = {}
+	el.onclick = () => {
+		for (const i in Object.keys(currentDLObject)) {
+			if (keys[i] !== 'click' && keys[i] !== 'hover')
+				requiredDLObject[keys[i]] = values[i]
+		}
+		requiredDLObject.event = 'click'
+		handleSetOnDataLayer(requiredDLObject)
+	}
+}
+
+const handleMouseOverObserver = (el: HTMLElement, currentDLObject: any) => {
+	const keys = Object.keys(currentDLObject)
+	const values = Object.values(currentDLObject)
+	const requiredDLObject: any = {}
+	el.onmouseover = () => {
+		for (const i in Object.keys(currentDLObject)) {
+			if (keys[i] !== 'click' && keys[i] !== 'hover')
+				requiredDLObject[keys[i]] = values[i]
+		}
+		requiredDLObject.event = 'mouseOver'
+		handleSetOnDataLayer(requiredDLObject)
+	}
+}
+
+const handleComposeDLObject = (el: HTMLElement, customDLObject: any) => {
+	const defaultDLObject = {
+		click: true,
+		hover: false,
+		action: '',
+		target: el,
+		category: el.tagName.toLowerCase(),
+		page: el.baseURI,
+	}
+	return Object.assign(defaultDLObject, customDLObject)
+}
+
+Vue.directive('DLObject', {
+	bind(el: any, { value }: any): void {
+		const customDLObject = value
+		const currentDLObject = handleComposeDLObject(el, customDLObject)
+		if (currentDLObject !== null && currentDLObject.click === true)
+			handleClickObserver(el, currentDLObject)
+		if (currentDLObject !== null && currentDLObject.hover === true)
+			handleMouseOverObserver(el, currentDLObject)
+	},
+})
